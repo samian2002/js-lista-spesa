c9a40f1c7b0f29e2e69e035ac3f0a858abf55eaf
samian2002 <samiantariq14@gmail.com>
2024-04-29 17:29:14 +0200

    M3 stampa degli elemnti in pagina

--- scripts/main.js
+++ scripts/main.js
@@ -29,9 +29,20 @@ const listaSpesa = [
 /*for (let i = 0; i < listaSpesa.length; i++) {
     console.log(listaSpesa[i]);
 }*/
+
 //Ciclo while per stampare in console
-let i = 0;
+/*let i = 0;
 while (i < listaSpesa.length) {
     console.log(listaSpesa[i]);
+    i++;
+}*/
+
+let i = 0;
+const ulElement = document.getElementById('listaSpesa');
+
+while (i < listaSpesa.length) {
+    const li = document.createElement('li'); // Creazione di un nuovo elemento li
+    li.textContent = listaSpesa[i]; // Impostazione del testo dell'elemento li
+    ulElement.appendChild(li); 
     i++;
 }
