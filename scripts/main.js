d28ff086d284fa64d441549fbbad484d8aa9e09e
samian2002 <samiantariq14@gmail.com>
2024-04-29 17:13:55 +0200

    m1 creo array

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+//Creazione dell'array con la lista della spesa
+const listaSpesa = [
+    "pane",
+    "insalata",
+    "mandarini",
+    "frutta",
+    "lattuga",
+    "manico della scopa",
+    "olive",
+    "piadine",
+    "cotto",
+    "speck",
+    "farina",
+    "salame",
+    "carciofi",
+    "asparagi",
+    "olio",
+    "sgrassatore",
+    "dentifircio",
+    "tabacco",
+    "pepe",
+    "carne",
+    "coltello piccolo",
+    "patatine",
+    "spazzolini",
+    "ammorbidente"
+]
